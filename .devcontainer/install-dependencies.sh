d66e238c57d3b22311c8b0c9ba56ecb8cf1e3883
miekki <maciejws@gmail.com>
2025-05-11 11:31:15 +0000

    Update dev container

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -14,3 +14,11 @@ sudo apt-get autoremove -y
 sudo apt-get autoclean
 
 echo "System update completed!"
+
+echo "Installing ngrok - revers proxy need it for gh webhooks"
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
